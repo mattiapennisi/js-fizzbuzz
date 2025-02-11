d1179aa2da9857cfc46db473eb3ac80cedbee8c3
mattiapennisi <mattiapennisi0@gmail.com>
2025-02-11 13:46:26 +0100

    fix script.js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 // Consegna:
-// Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz. Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+// Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz. Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz. 
 
 // Loop for per contare da 1 a 100
 for (let i = 1; i <= 100; i++) {
+    // If statement per stampare "FizzBuzz" quando il contatore ha un numero moltiplicabile per 3 e per 5
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("FizzBuzz");
 
     // If statement per stampare "Fizz" quando il contatore ha un numero moltiplicabile per 3
-    if (i % 3 == 0) {
+    } else if (i % 3 == 0) {
         console.log("Fizz");
-    // If statement per stampare "Buzz" quando il contatore ha un numero moltiplicabile per 5
+        
+    // If statement per stampare "Buzz" quando il contatore ha un numero moltiplicabile per 5 e per 3
     } else if (i % 5 == 0) {
         console.log("Buzz");
     // Negli altri casi stampo solo il numero
