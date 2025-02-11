0a1dd630250a88c2f5fdfe83dad87feecf98b0c0
mattiapennisi <mattiapennisi0@gmail.com>
2025-02-11 13:27:49 +0100

    complete fizzbuzz

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 // Consegna:
 // Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz. Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
-// Prima di partire a scrivere codice poniamoci qualche domanda:
-// Come faccio a sapere se un numero è divisibile per?
-// Abbiamo visto qualcosa di particolare che possiamo usare?
-// In che ordine devo mettere le condizioni?
-// Consigli del giorno:
-// scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
-// proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
 
+// Loop for per contare da 1 a 100
+for (let i = 1; i <= 100; i++) {
+
+    // If statement per stampare "Fizz" quando il contatore ha un numero moltiplicabile per 3
+    if (i % 3 == 0) {
+        console.log("Fizz");
+    // If statement per stampare "Buzz" quando il contatore ha un numero moltiplicabile per 5
+    } else if (i % 5 == 0) {
+        console.log("Buzz");
+    // Negli altri casi stampo solo il numero
+    } else {
+        console.log(i);
+    }
+}
